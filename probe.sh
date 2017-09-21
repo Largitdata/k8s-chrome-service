dcde946c83a6b819886bd11887b7b4fc1713e940
Brian Luan <jznight@gmail.com>
2017-09-21 17:49:39 +0800

    handle zombie processes for health checking

--- probe.sh
+++ probe.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
 
-CNT=`ps -ef|grep chrome|egrep -v "chromedriver|grep"|wc -l`
-if [ "$CNT" -gt "20" ];
+CNT=`ps -ef|grep chrome.*proxy|egrep -v "chromedriver|grep"|wc -l`
+if [ "$CNT" -gt "30" ];
 then
-    echo "Too many chrome process are running.."
+    echo "Too many chrome processes are running.." >>/dev/stderr
+    echo "restaring in 20 seconds.." >>/dev/stderr
+    sleep 20
     exit 100
+fi
+
+CNT=`ps -ef|grep "\[chrome\]"|wc -l`
+if [ "$CNT" -gt "5" ];
+then
+    echo "Too many zombie processes.." >>/dev/stderr
+    CNT=`ps -ef|grep chrome.*proxy|egrep -v "chromedriver|grep"|wc -l`
+    if [ "$CNT" -gt "1" ];
+    then
+        echo "restaring in 20 seconds.." >>/dev/stderr
+        sleep 20
+    fi
+    exit 200
 fi
--- ready-probe.sh
+++ ready-probe.sh
@@ -3,6 +3,6 @@
 netstat -na|grep 0.0.0.0:9515
 if [ $? -ne 0 ];
 then
-    echo "chromedriver is not running.."
+    echo "chromedriver is not running.." >>/dev/stderr
     exit 100
 fi
