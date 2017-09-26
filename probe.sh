cbb2b5ab03e5b1292cb86d9d8b262c6e1f5fe9e8
Brian Luan <jznight@gmail.com>
2017-09-26 16:09:12 +0800

    fix bug when kill long running chrome process

--- probe.sh
+++ probe.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ps -eo pid,comm,etime |grep " chrome "|awk '{split($3,a,":"); if(int(a[1])>10) print $1 }'|xargs - n 1 kill -9
+ps -eo pid,comm,etime |grep " chrome "|awk '{split($3,a,":"); if(int(a[1])>10) print $1 }'|xargs -n 1 kill -9
 
 CNT=`ps -ef|grep chrome.*proxy|egrep -v "chromedriver|grep"|wc -l`
 if [ "$CNT" -gt "30" ];
